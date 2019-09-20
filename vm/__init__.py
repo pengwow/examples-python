ea3f5aaf03cc78e1189e5fa0c50b2a7327a03990
liupeng <pengwow@hotmail.com>
2019-09-20 11:31:32 +0800

    update

--- vm/__init__.py
+++ vm/__init__.py
@@ -1,10 +1,16 @@
-from collections import Counter
-ddd = ['22222','33333','44444','22222']
-aaa = ['aaaaa','aaaaa','ddddd','aaaaa']
+import json
 
+test_str = {
+    'serial_number': '817204284',
+    'cpu_model_name': 'Intel(R) Xeon(R) CPU E5-2630 v4 @ 2.20GHz',
+    'mfg': 'Inspur',
+    'product': 'NF5180M4 (Default string)',
+    'cpu_s': '40',
+    'memory': '16384 MB * 12',
+    'network_card': 'I350 Gigabit Network Connection * 2,Ethernet Controller 10-Gigabit X540-AT2 * 4',
+    'total_disk': 'SCSI Disk/837GiB (899GB) * 1'
+  }
 
-fff = list(zip(aaa,ddd))
-
-xxx = Counter(fff)
-print(xxx)
+ddd = json.dumps(test_str)
+print(ddd)
 
