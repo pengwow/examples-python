a74a3ded108089c5a41486db3fedffc92b479fa8
liupeng <pengwow@hotmail.com>
2019-11-25 10:50:32 +0800

    update

---
+++ file/walkfile.py
@@ -0,0 +1,27 @@
+import os
+
+
+# 遍历文件夹
+def walkFile(file):
+    for root, dirs, files in os.walk(file):
+
+        # root 表示当前正在访问的文件夹路径
+        # dirs 表示该文件夹下的子目录名list
+        # files 表示该文件夹下的文件list
+
+        # 遍历文件
+        for f in files:
+            print(os.path.join(root, f))
+
+        # 遍历所有的文件夹
+        for d in dirs:
+            print(os.path.join(root, d))
+
+
+def main():
+    walkFile("E:\\data\\固件包\\联想")
+
+
+if __name__ == '__main__':
+    main()
+# os.path.normcase(p1) == os.path.normcase(p2)
---
+++ http/url.py
@@ -0,0 +1,5 @@
+import requests
+
+url = 'http://192.168.1.101:800/soft/list'
+
+requests.post(url,)
